fd2652e8c7b8938ca1b2c9f0775e2afe87a9b2cf
dylan12manley <dylan.manley@gmail.com>
2024-09-21 12:09:43 -0700

    update styling and add image

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Landing msg="Muilenburg Contracting"/>
+  <HomePage msg="Muilenburg Contracting"/>
 </template>
 
 <script>
-import Landing from './components/Landing.vue'
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    Landing
+    HomePage
   }
 }
 </script>
 
 <style>
+body { 
+  margin: 0; 
+  height: 100%;
+  color: #FFE4AD;
+  background-color: #323031;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <span>Zack Muilenburg Contracting</span>
+    <img src='@/assets/images/selfie.png' alt='dylan selfie'/>
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    height: 100%;
+  }
+</style>
