7a6af1c69873a6591e666b2b684dd65cf6eb13cc
dylan12manley <dylan.manley@gmail.com>
2024-09-21 12:09:09 -0700

    create file

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about">
+      <span>I started working in construction in 2012.</span>
+      <img src='@/assets/images/selfie.png'/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutUs',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+    .about {
+        
+    }
+  </style>
+  
